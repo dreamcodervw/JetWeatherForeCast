76c66a0601d8081c03787a0930abbe9d055caf79
Milan Bhatt <dreamcodervw@gmail.com>
2025-10-26 16:59:04 +0530

    Created basic navigation structure

---
+++ app/src/main/java/com/dreamcodervw/jetweatherforecast/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.dreamcodervw.jetweatherforecast.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavouriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
